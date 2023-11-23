670ea7752af3ccdecdfbd41bd9c4204eabbae806
Vanessa Rebecca <64979714+vanessrw@users.noreply.github.com>
2023-11-23 20:05:48 +0700

    fix: connection ke booking

--- src/components/InputCluster/InputCluster.tsx
+++ src/components/InputCluster/InputCluster.tsx
@@ -1,69 +1,84 @@
+// InputCluster.tsx
 import { useState } from 'react';
 import { useCookies } from 'react-cookie';
 
-interface InputCluster{
-    placeholder: string;
-    buttonText: string;
-    serverURL: string;
-    onRequestSuccess: () => void;
+interface InputCluster {
+  placeholder: string;
+  buttonText: string;
+  serverURL: string;
+  onRequestSuccess: () => void;
+  onClick: () => Promise<void>;
 }
 
-const InputCluster: React.FC<InputCluster> = ({ placeholder: title, buttonText, serverURL }) => {
-    const [inputData, setInputData] = useState('');
-    const [cookies, setCookie] = useCookies(['token']);
-    const [logMessage, setLogMessage] = useState<string | null>(null);
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+const InputCluster: React.FC<InputCluster> = ({
+  placeholder: title,
+  buttonText,
+  serverURL,
+  onRequestSuccess,
+  onClick,
+}) => {
+  const [inputData, setInputData] = useState('');
+  const [cookies, setCookie] = useCookies(['token']);
+  const [loading, setLoading] = useState(false);
+  const [logMessage, setLogMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-    const headers: HeadersInit = {
-        "Content-Type": "application/json",
-    };
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+  };
   
-    if (cookies.token) {
-        headers["Authorization"] = `Bearer ${cookies.token}`;
+  if (cookies.token) {
+    headers["Authorization"] = `Bearer ${cookies.token}`;
+  }
+
+  const sendData = async () => {
+    setLogMessage(null);
+    setErrorMessage(null);
+    setLoading(true);
+  
+    const parsedInputData = parseInt(inputData, 10);
+  
+    if (isNaN(parsedInputData)) {
+      setErrorMessage('Error: Input data must be a valid integer');
+      setLoading(false);
+      return;
     }
-
-    const sendData = () => {
-        setLogMessage(null);
-        setErrorMessage(null);
-
-        const parsedInputData = parseInt(inputData, 10);
-
-        if(isNaN(parsedInputData)){
-            setErrorMessage('Error: Input data must be a valid integer');
-            return;
-        }
-
-        setLogMessage(`Fetching...`);
-        fetch(serverURL, {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify({ inputData }),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log("Server response:", data);
-                setLogMessage(`Success, server response: ${data}`);
-            })
-            .catch(error => {
-                console.error("Request error", error);
-                setLogMessage(null);
-                setErrorMessage(`Error, server response: ${error.message}`);
-            })
+  
+    setLogMessage('Fetching...');
+    
+    try {
+      await onClick(); 
+      onRequestSuccess(); // Update state on success
+      setLogMessage('Success'); // Update log message
+    } catch (error: any) {
+      console.error('Request error', error);
+      setErrorMessage(`Error: ${error.message}`);
+    } finally {
+      setLoading(false);
     }
+  };
+  
 
-    return(
-        <div>
-            <input
-            type="text"
-            placeholder={title}
-            value={inputData}
-            onChange={(e) => setInputData(e.target.value)}
-            />
-            <button onClick={sendData} className='ButtonGeneric' style={{marginLeft: "10px", padding: "3px 10px"}}>{buttonText}</button>
-            {logMessage && <p style={{ color: 'green' }}>{logMessage}</p>}
-            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        </div>
-    )
-}
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder={title}
+        value={inputData}
+        onChange={(e) => setInputData(e.target.value)}
+      />
+      <button
+        onClick={sendData}
+        className={`ButtonGeneric ${loading ? 'disabled' : ''}`}
+        style={{ marginLeft: '10px', padding: '3px 10px' }}
+        disabled={loading}
+      >
+        {loading ? 'Fetching...' : buttonText}
+      </button>
+      {logMessage && <p style={{ color: 'green' }}>{logMessage}</p>}
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+    </div>
+  );
+};
 
 export default InputCluster;
--- src/views/BookingPage.tsx
+++ src/views/BookingPage.tsx
@@ -1,7 +1,9 @@
+// BookingPage.tsx
 import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import InputCluster from '../components/InputCluster/InputCluster';
 import { useState } from 'react';
+import { bookKursi } from '../api'; 
 
 const BookingPage: React.FC = () => {
   const [requestSuccess, setRequestSuccess] = useState<boolean>(false);
@@ -10,16 +12,30 @@ const BookingPage: React.FC = () => {
     setRequestSuccess(true);
   };
 
+  const handleBookKursi = async () => {
+    const acaraId = 1;
+    const kursiId = 1;
+    const userId = 1;
+
+    try {
+      await bookKursi(acaraId, kursiId, userId);
+      handleRequestSuccess(); // Update state on success
+    } catch (error) {
+      console.error('Error booking kursi:', error);
+    }
+  };
+  
   return (
     <div className="App">
-      <Header title="Booking"/>
+      <Header title="Booking" />
       <div className='Content'>Booking ceritanya</div>
       <InputCluster
         placeholder='Booking'
         buttonText='Send Request'
-        serverURL='http://localhost:8080'
-        onRequestSuccess={handleRequestSuccess}
-        />
+        serverURL='http://localhost:3100'
+        onClick={handleBookKursi} 
+        onRequestSuccess={() => handleRequestSuccess()} 
+      />
       {requestSuccess && (
         <Link to="/payment" className='ButtonGeneric' style={{ marginTop: '10px' }}>
           Payment
